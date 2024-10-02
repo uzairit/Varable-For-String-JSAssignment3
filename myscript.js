8d2fd8460dcdc685fe3077e5ceabe2fb32c0d275
Uzair Raza <uzair18it@gmail.com>
2024-10-03 04:59:44 +0500

    Assignment 3

---
+++ myscript.js
@@ -0,0 +1,26 @@
+var userName ="Uzair";
+console.log(userName);
+
+var myName="Uzair Raza";
+console.log(myName);
+
+var message="Hello";
+console.log(message);
+
+alert("Hello");
+
+var studentName="Ubaid" ,age="10" ,Qualification="Web Development";
+alert(studentName);
+alert(age);
+alert(Qualification);
+
+var pizza="Pizza\npizz\npiz\npi\np";
+alert(pizza);
+
+var email="My email address is example@example.com";
+alert(email);
+
+var book="I am trying to learn from the book a smarter way to learn Javascript";
+alert(book);
+
+document.write("I can write html content through javascript");
